dd894f8df5cb653fb866f7e8939d21df3d8412ad
Mahmoud3Ibrahim <mahmoud.hassan.img@gmail.com>
2025-07-11 15:37:31 -0400

    commit

--- Power_BI_Script.R
+++ Power_BI_Script.R
@@ -1,12 +1,15 @@
 library(tidyverse)
 library(lubridate)
 
-df <- read_csv("data/processed/housing_clean_final.csv")
+# Read the cleaned file from the correct path
+df <- read_csv("D:/College/projects/canadian-housing-prices-analysis--R-PowerBI-/data/processed/housing_clean_final.csv")
 
+# Prepare data for Power BI
 df_pbi <- df %>%
   filter(Type == "Total (house and land)") %>%
   mutate(Year = year(Date)) %>%
   group_by(Geography, Year) %>%
-  summarize(Avg_Value = mean(Value, na.rm = TRUE), .groups = "drop")
+  summarize(Avg_Value = round(mean(Value, na.rm = TRUE), 2), .groups = "drop")
 
-write_csv(df_pbi, "/output/summary_tables/housing_for_powerbi.csv")
+# Save the new CSV file
+write_csv(df_pbi, "D:/College/projects/canadian-housing-prices-analysis--R-PowerBI-/data/processed/housing_for_powerbi.csv")
